781d90bf7c1d95194780af5aa60b7d430b10843e
Matthew Campbell <cattmampbell@gmail.com>
2018-12-08 14:25:46 -0500

    Created countdown() function via app.js file.

--- app.js
+++ app.js
@@ -1,50 +1,180 @@
-/* Creates DOMContentLoaded Event Handler */
-document.addEventListener("DOMContentLoaded", function(event) {
-    /* Should log: Success, DOM fully loaded and parsed! */
-    console.log("%cSuccess,%c DOM fully loaded and parsed!", "font-size: 12px; font-weight: 900; color: #28a745;", "font-size: 12px; font-weight: 500; color: #28a745;"); 
+// Should log: Start of app.js file! 
+// console.log(
+//     "%cStart%c of app.js file!", 
+//     "font-size: 12px; font-weight: 900; color: #007bff;", 
+//     "font-size: 12px; font-weight: 500; color: #007bff;"
+// )
 
-    /* Should log: Start of app.js file! */
-    console.log("%cStart%c of app.js file!", "font-size: 12px; font-weight: 900; color: #007bff;", "font-size: 12px; font-weight: 500; color: #007bff;"); 
+// Creates DOMContentLoaded event handler
+document.addEventListener("DOMContentLoaded", (event) => {
+    console.log(
+        "%cSuccess,%c DOM fully loaded and parsed!", 
+        "font-size: 12px; font-weight: 900; color: #28a745;", 
+        "font-size: 12px; font-weight: 500; color: #28a745;"
+    )
 
-    /* Defines Global Variables */
+    // Defines global variables
     let number = Math.floor(Math.random() * (11 - 1) + 1);
-    console.log(`%cThe random number is %c${number}.`, "font-size: 12px; font-weight: 500; color: #6c757d;", "font-size: 12px; font-weight: 900; color: #6c757d;");
+    const boolean = true;
     
-    /* Creates print() Function */
-    function print(message) {
-        console.log(`%c${message}`, "font-size: 12px; font-weight: 900; color: #343a40;");
-        return number;
-    };
-
-    /* Creates multiply() Function */
-    function multiply() {
-        console.log(`%c${number} %cmultiplied by %c4%c is %c${number * 4}.`, "font-size: 12px; font-weight: 900; color: #6c757d;", "font-size: 12px; font-weight: 500; color: #6c757d;", "font-size: 12px; font-weight: 900; color: #6c757d;", "font-size: 12px; font-weight: 500; color: #6c757d;", "font-size: 12px; font-weight: 900; color: #6c757d;");
+    // Creates print() function 
+    // Accepts an argument named 'message' 
+    const print = (message) => {
+        console.log(
+            `%c${message}`, 
+            "font-size: 12px; font-weight: 900; color: #007bff;"
+        )
+        return console.log(
+            `%cThe random number is %c${number}.`, 
+            "font-size: 12px; font-weight: 500; color: #6c757d;", 
+            "font-size: 12px; font-weight: 900; color: #6c757d;"
+        )
     }
 
-    /* Calls multiply() Function after 2 seconds via setTimeout()Function */
+    // Calls print() function
+    print("Hello World!");
+
+    // Creates multiply() function
+    const multiply = () => {
+        let multiplyNumber = number * 4;
+        console.log(
+            `%c${number} %cmultiplied by %c4%c is %c${multiplyNumber}`, 
+            "font-size: 12px; font-weight: 900; color: #6c757d;", 
+            "font-size: 12px; font-weight: 500; color: #6c757d;", 
+            "font-size: 12px; font-weight: 900; color: #6c757d;", 
+            "font-size: 12px; font-weight: 500; color: #6c757d;", 
+            "font-size: 12px; font-weight: 900; color: #6c757d;"
+        )
+        console.log(
+            "   %cDelay:%c 2%c seconds",
+            "font-size: 10px; font-weight: 500; color: #6c757d;; text-decoration: underline;",
+            "font-size: 10px; font-weight: 900; color: #6c757d;",
+            "font-size: 10px; font-weight: 500; color: #6c757d;"
+        )
+    }
+
+    // Calls multiply() function after waiting 2 seconds via setTimeout() function 
     setTimeout(multiply, 2000);
 
-    /* Creates getWords() Function */
-    function getWords(word1, word2, word3, word4) {
-        console.log(`%c1st Word: %c${word1}`, "font-size: 12px; font-weight: 900; color: #343a40;", "font-size: 12px; font-weight: 500; color: #343a40;");
-        setTimeout(function() {
-            console.log(`%c2nd Word: %c${word2}`, "font-size: 12px; font-weight: 900; color: #343a40;", "font-size: 12px; font-weight: 500; color: #343a40;");
-        }, 3000);
-        setTimeout(function() {
-            console.log(`%c3rd Word: %c${word3}`, "font-size: 12px; font-weight: 900; color: #343a40;", "font-size: 12px; font-weight: 500; color: #343a40;");
-        }, 2000);
-        setTimeout(function() {
-            console.log(`%c4th Word: %c${word4}`, "font-size: 12px; font-weight: 900; color: #343a40;", "font-size: 12px; font-weight: 500; color: #343a40;");
-        }, 1000);
-    };
+    // Creates getWords() function 
+    // Accepts 4 arguments called 'word1', 'word2', 'word3' and 'word4'
+    const getWords = (word1, word2, word3, word4) => {
+        // Logs word1 to the console immediately 
+        console.log(
+            `%c1st Word: %c${word1}`, 
+            "font-size: 12px; font-weight: 900; color: #343a40;", 
+            "font-size: 12px; font-weight: 500; color: #343a40;"
+        )
+        console.log(
+            "   %cDelay:%c 0%c seconds",
+            "font-size: 10px; font-weight: 500; color: #343a40; text-decoration: underline;",
+            "font-size: 10px; font-weight: 900; color: #343a40;",
+            "font-size: 10px; font-weight: 500; color: #343a40;"
+        )
+        // Logs word2 to the console after waiting 3 seconds via setTimeout() function 
+        setTimeout(() => {
+            console.log(
+                `%c2nd Word: %c${word2}`, 
+                "font-size: 12px; font-weight: 900; color: #343a40;", 
+                "font-size: 12px; font-weight: 500; color: #343a40;"
+            )
+            console.log(
+                "   %cDelay:%c 3%c seconds",
+                "font-size: 10px; font-weight: 500; color: #343a40; text-decoration: underline;",
+                "font-size: 10px; font-weight: 900; color: #343a40;",
+                "font-size: 10px; font-weight: 500; color: #343a40;"
+            )
+        }, 3000)
+        // Logs word3 to the console after waiting 2 seconds via setTimeout() function 
+        setTimeout(() => {
+            console.log(
+                `%c3rd Word: %c${word3}`, 
+                "font-size: 12px; font-weight: 900; color: #343a40;", 
+                "font-size: 12px; font-weight: 500; color: #343a40;"
+            )
+            console.log(
+                "   %cDelay:%c 2%c seconds",
+                "font-size: 10px; font-weight: 500; color: #343a40; text-decoration: underline;",
+                "font-size: 10px; font-weight: 900; color: #343a40;",
+                "font-size: 10px; font-weight: 500; color: #343a40;"
+            )
+        }, 2000)
+        // Logs word1 to the console after waiting 1 second via setTimeout() function 
+        setTimeout(() => {
+            console.log(
+                `%c4th Word: %c${word4}`, 
+                "font-size: 12px; font-weight: 900; color: #343a40;", 
+                "font-size: 12px; font-weight: 500; color: #343a40;"
+            )
+            console.log(
+                "   %cDelay:%c 1%c second",
+                "font-size: 10px; font-weight: 500; color: #343a40; text-decoration: underline;",
+                "font-size: 10px; font-weight: 900; color: #343a40;",
+                "font-size: 10px; font-weight: 500; color: #343a40;"
+            )
+        }, 1000)
+    }
 
-    /* Calls getWords() Function */
-    /* Should log: 1st Word: Hello */
-    /* Should log: 4th Word: Adiós */
-    /* Should log: 3rd Word: Hola */
-    /* Should log: 2nd Word: Goodbye */
+    // Calls getWords() Function 
+    // Should log: 
+    // 1st Word: Hello 
+    // 4th Word: Adiós 
+    // 3rd Word: Hola 
+    // 2nd Word: Goodbye 
     getWords("Hello", "Goodbye", "Hola", "Adiós");
 
-    /* Should log: End of app.js file! */
-    console.log("%cEnd%c of app.js file!", "font-size: 12px; font-weight: 900; color: #007bff;", "font-size: 12px; font-weight: 500; color: #007bff;"); 
+    // Creates done() Function 
+    // Accepts Argument called 'message' 
+    const done = (message) => {
+        console.log(
+            `%c${message}`, 
+            "font-size: 14px; font-weight: 900; color: #28a745;"
+        )
+    }
+
+    // Creates countdown() Function 
+    // Accepts Arguments called 'start' and 'callback' 
+    const countdown = (start, callback) => {
+        console.log(
+            `%cCountdown%c: %c${start}%c seconds`, 
+            "font-size: 12px; font-weight: 700; color: #343a40; text-decoration: underline;",
+            "font-size: 12px; font-weight: 600; color: #343a40;", 
+            "font-size: 12px; font-weight: 700; color: #dc3545;",
+            "font-size: 12px; font-weight: 500; color: #dc3545;"
+        )
+        start -= 1;
+        let timer = setInterval(() => {
+            if(start > 1) {
+                console.log(
+                    `%cCountdown%c: %c${start}%c seconds`, 
+                    "font-size: 12px; font-weight: 700; color: #343a40; text-decoration: underline;",
+                    "font-size: 12px; font-weight: 600; color: #343a40;", 
+                    "font-size: 12px; font-weight: 700; color: #dc3545;",
+                    "font-size: 12px; font-weight: 500; color: #dc3545;"
+                )
+                start -= 1;
+            } else if(start === 1) {
+                console.log(
+                    `%cCountdown%c: %c${start}%c second`, 
+                    "font-size: 12px; font-weight: 700; color: #343a40; text-decoration: underline;",
+                    "font-size: 12px; font-weight: 600; color: #343a40;", 
+                    "font-size: 12px; font-weight: 700; color: #dc3545;",
+                    "font-size: 12px; font-weight: 500; color: #dc3545;"
+                )
+                start -= 1;
+            } else if(start === 0) {
+                callback("Done!");
+                clearInterval(timer);
+            }
+        }, 1000)
+    }
+
+    countdown(10, done);
 });
+
+// Should log: End of app.js file! 
+// console.log(
+//     "%cEnd%c of app.js file!", 
+//     "font-size: 12px; font-weight: 900; color: #007bff;", 
+//     "font-size: 12px; font-weight: 500; color: #007bff;"
+// )
